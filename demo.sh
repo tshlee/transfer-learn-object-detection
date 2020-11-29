66eb920eeb14fdf7616b6e7000a1fd6b423b12d4
tshlee <gtshlee@gmail.com>
2020-11-29 12:18:21 -0500

    code for data preprocessing

--- demo.sh
+++ demo.sh
@@ -3,17 +3,31 @@ MODEL_DIR=/path/to/model
 #CODE_DIR=.
 CODE_DIR=/path/to/research/object_detection
 
-TRAIN_IMAGE_PATH=$DATA_DIR/my_data/images/train  #my-demo/images/train #incl *.xml
+TRAIN_IMAGE_PATH=$DATA_DIR/my_data/images/train
 TEST_IMAGE_PATH=$DATA_DIR/my_data/images/test
-ANNOTATION_PATH=$DATA_DIR/my_data/annotations  #*.csv *.record
 
-MODEL_PATH=$MODEL_DIR/my_model  #my-demo/models/my_model
-SOURCE_MODEL_PATH=$MODEL_DIR/pretrained_model  #my-demo/pre-trained-models
-EXPORTED_MODEL_PATH=$MODEL_DIR/my_exported_model  #my-demo/exported-models
+ANNOTATION_PATH=$DATA_DIR/my_data/annotations
+LABELMAP_PATH=$ANNOTATION_PATH/label_map.pbtxt
+CLASS_NAME='myLabel'
+TRAIN_TFRECORD_PATH=$ANNOTATION_PATH/train.record
+TEST_TFRECORD_PATH=$ANNOTATION_PATH/test.record
+
+MODEL_PATH=$MODEL_DIR/my_model
+PIPELINE_PATH=$MODEL_PATH/pipeline.config
+
+SOURCE_MODEL_PATH=$MODEL_DIR/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8
+SOURCE_PIPELINE_PATH=$SOURCE_MODEL_PATH/pipeline.config
+
+EXPORTED_MODEL_PATH=$MODEL_DIR/my_exported_model
 
 TRAIN_PY=$CODE_DIR/model_main_tf2.py
 EXPORT_PY=$CODE_DIR/exporter_main_v2.py
 
+python demo_config.py $LABELMAP_PATH $CLASS_NAME $PIPELINE_PATH $SOURCE_MODEL_PATH $SOURCE_PIPELINE_PATH $TRAIN_TFRECORD_PATH $TEST_TFRECORD_PATH
+python generate_tfrecord.py -x $TRAIN_IMAGE_PATH -l $LABELMAP_PATH -o $TRAIN_TFRECORD_PATH
+python generate_tfrecord.py -x $TEST_IMAGE_PATH -l $LABELMAP_PATH -o $TEST_TFRECORD_PATH
+
 python $TRAIN_PY --model_dir=$MODEL_PATH --pipeline_config_path=$MODEL_PATH/pipeline.config
+
 python $EXPORT_PY --input_type=image_tensor --pipeline_config_path=$MODEL_PATH/pipeline.config --trained_checkpoint_dir $MODEL_PATH --output_directory $EXPORTED_MODEL_PATH
 python demo_detect.py $EXPORTED_MODEL_PATH
---
+++ demo_config.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from google.protobuf import text_format
+from object_detection.protos.pipeline_pb2 import TrainEvalPipelineConfig 
+from object_detection.protos.string_int_label_map_pb2 import StringIntLabelMap, StringIntLabelMapItem
+
+LABELMAP_PATH, CLASS_NAME, PIPELINE_PATH, SOURCE_MODEL_PATH, SOURCE_PIPELINE_PATH, TRAIN_TFRECORD_PATH, TEST_TFRECORD_PATH = sys.argv[1:8]
+
+#Save labelmap
+labelmap = StringIntLabelMap()
+labelmap.item.append(StringIntLabelMapItem(id=1, name=CLASS_NAME))
+with open(LABELMAP_PATH, 'w') as f:
+    f.write(text_format.MessageToString(labelmap))
+
+#Edit source pipeline and save as pipeline
+pipeline = TrainEvalPipelineConfig()
+with open(SOURCE_PIPELINE_PATH, 'r') as f:
+    text_format.Merge(f.read(), pipeline)
+pipeline.model.ssd.num_classes = 1
+pipeline.train_input_reader[0].label_map_path = LABELMAP_PATH
+pipeline.train_input_reader[0].tf_record_input_reader.input_path[0] = TRAIN_TFRECORD_PATH
+pipeline.train_config.fine_tune_checkpoint = os.path.join(SOURCE_MODEL_PATH, 'checkpoint/ckpt-0')
+pipeline.train_config.fine_tune_checkpoint_type = 'detection'
+pipeline.train_config.batch_size = 4
+pipeline.train_config.use_bfloat16 = False
+pipeline.eval_input_reader[0].label_map_path = LABELMAP_PATH
+pipeline.eval_input_reader[0].tf_record_input_reader.input_path[0] = TEST_TFRECORD_PATH
+pipeline.eval_config.metrics_set = 'coco_detection_metrics'
+pipeline.eval_config.use_moving_averages = False
+with open(PIPELINE_PATH, 'w') as f:
+    f.write(text_format.MessageToString(pipeline))
